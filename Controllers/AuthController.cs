0275bcbfef9c4a9dde40f5dae3bf21682821c85a
Lime <demn2001@gmail.com>
2022-12-25 23:31:32 +0300

    Добавьте файлы проекта.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebGP.Controllers
+{
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+
+    }
+}
---
+++ Models/JwtConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace WebGP.Models
+{
+    public class JwtConfig
+    {
+#pragma warning disable CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
+        public string ISSUER { get; set; }
+        public string AUDIENCE { get; set; }
+        public string KEY { get; set; }
+#pragma warning restore CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
+
+        public SymmetricSecurityKey GetSymmetricSecurityKey() => new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY ?? ""));
+    }
+}
