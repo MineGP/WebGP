67bfbc447a36d60c94c4396d21e66efe5fb21aeb
Lime <demn2001@gmail.com>
2022-12-28 01:31:01 +0300

    Add Mapper

---
+++ WebGP.Application/Common/Mapper/MapperExt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebGP.Application.Common.VM;
+using WebGP.Domain.Entities;
+
+namespace WebGP.Application.Common.Mapper
+{
+    public static partial class MapperExt
+    {
+        public static OnlineLogVM MapToVM(this OnlineLog value) => new OnlineLogVM()
+        {
+            Day = value.Day.ToString("yyyy-MM-dd"),
+            Seconds = value.Seconds
+        };
+    }
+}
