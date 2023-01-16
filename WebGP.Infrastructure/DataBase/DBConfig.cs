db277785f6968054b9b12c3747071e22cdf377c4
Lime <demn2001@gmail.com>
2023-01-16 19:06:15 +0300

    `Server` property & update connection string builder

--- WebGP.Infrastructure/DataBase/DBConfig.cs
+++ WebGP.Infrastructure/DataBase/DBConfig.cs
@@ -8,11 +8,12 @@ public class DBConfig : IDBConfig
     public string User { get; set; } = null!;
     public string Password { get; set; } = null!;
     public string DataBase { get; set; } = null!;
+    public string Server { get; set; } = null!;
     public uint Port { get; set; }
 
     public string GetConnectionString() => new MySqlConnectionStringBuilder
     {
-        Server = "127.0.0.1",
+        Server = Server,
         Port = Port,
         Database = DataBase,
         UserID = User,
