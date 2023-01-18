03199a21c5d269751ad9569282f3373d73d4bd27
godunko-mikhail <97464195+godunko-mikhail@users.noreply.github.com>
2023-01-18 22:26:07 +0300

    add: MediatR request for new token by

---
+++ WebGP.Application/Tokens/Queries/CreateNewToken/CreateNewTokenQuery.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using WebGP.Application.Common.Interfaces;
+
+namespace WebGP.Application.Tokens.Queries.CreateNewToken
+{
+    public class CreateNewTokenQuery : IRequest<string>
+    {
+        public IEnumerable<string> Roles { get; set; } = null!;
+        public int Days { get; set; }
+    }
+    public class CreateNewTokenQueryHandler : IRequestHandler<CreateNewTokenQuery, string>
+    {
+        private readonly IJwtService _jwtService;
+        public CreateNewTokenQueryHandler(IJwtService jwtService)
+        {
+            _jwtService = jwtService;
+        }
+        public Task<string> Handle(CreateNewTokenQuery request, CancellationToken cancellationToken)
+        {
+            var builder = _jwtService.GetJwtBuilder();
+
+            foreach (var role in request.Roles)
+            {
+                builder.AddRole(role);
+            }
+
+            return Task.FromResult(builder.Build(TimeSpan.FromDays(request.Days)));
+        }
+    }
+}
