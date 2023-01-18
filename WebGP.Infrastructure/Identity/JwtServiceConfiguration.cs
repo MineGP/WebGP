7bfc3b353631449b33d57a770656d81a874c593e
godunko-mikhail <97464195+godunko-mikhail@users.noreply.github.com>
2023-01-18 22:19:24 +0300

    add: jwt service and builder

---
+++ WebGP.Infrastructure/Identity/JwtServiceConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebGP.Infrastructure.Identity
+{
+    public class JwtServiceConfiguration
+    {
+        public string Issuer { get; set; } = null!;
+        public string Audience { get; set; } = null!;
+        public string Key { get; set; } = null!;
+        public TimeSpan Expires { get; set; }
+
+        public SymmetricSecurityKey GetSecurityKey() => new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Key));
+    }
+}
