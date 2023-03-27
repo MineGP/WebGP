224a497a77e470123cfdf7944a80eeb08b148f2f
godunko-mikhail <97464195+godunko-mikhail@users.noreply.github.com>
2023-03-27 16:51:39 +0300

    fix nullable for not registred users

--- WebGP.Application/Common/VM/OnlineVM.cs
+++ WebGP.Application/Common/VM/OnlineVM.cs
@@ -4,14 +4,14 @@ namespace WebGP.Application.Common.VM;
 
 public class OnlineVm
 {
-    [JsonPropertyName("timed_id")] public int TimedId { get; set; }
-    [JsonPropertyName("uuid")] public string Uuid { get; set; } = null!;
-    [JsonPropertyName("static_id")] public uint StaticId { get; set; }
-    [JsonPropertyName("first_name")] public string FirstName { get; set; } = null!;
-    [JsonPropertyName("last_name")] public string LastName { get; set; } = null!;
+    [JsonPropertyName("timed_id")] public int? TimedId { get; set; }
+    [JsonPropertyName("uuid")] public string? Uuid { get; set; } = null!;
+    [JsonPropertyName("static_id")] public uint? StaticId { get; set; }
+    [JsonPropertyName("first_name")] public string? FirstName { get; set; } = null!;
+    [JsonPropertyName("last_name")] public string? LastName { get; set; } = null!;
     [JsonPropertyName("discord_id")] public long? DiscordId { get; set; }
-    [JsonPropertyName("role")] public string Role { get; set; } = null!;
-    [JsonPropertyName("work")] public string Work { get; set; } = null!;
+    [JsonPropertyName("role")] public string? Role { get; set; } = null!;
+    [JsonPropertyName("work")] public string? Work { get; set; } = null!;
     [JsonPropertyName("level")] public int Level { get; set; }
     [JsonPropertyName("skin_url")] public string SkinUrl { get; set; } = null!;
 }
