ebe1c1f6b37b28e3c3e6a3e623372672dcc77d84
godunko-mikhail <97464195+godunko-mikhail@users.noreply.github.com>
2023-04-01 11:47:54 +0300

    add: 4 fields for a discord vm response

--- WebGP.Application/Common/VM/DiscordVM.cs
+++ WebGP.Application/Common/VM/DiscordVM.cs
@@ -5,6 +5,10 @@ namespace WebGP.Application.Common.VM;
 public class DiscordVm
 {
     [JsonPropertyName("discord_id")] public long DiscordId { get; set; }
+    [JsonPropertyName("uuid")] public string? Uuid { get; set; } = null!;
+    [JsonPropertyName("static_id")] public uint? StaticId { get; set; }
+    [JsonPropertyName("first_name")] public string? FirstName { get; set; } = null!;
+    [JsonPropertyName("last_name")] public string? LastName { get; set; } = null!;
     [JsonPropertyName("work")] public string Work { get; set; } = null!;
     [JsonPropertyName("role")] public string Role { get; set; } = null!;
     [JsonPropertyName("level")] public int Level { get; set; }
