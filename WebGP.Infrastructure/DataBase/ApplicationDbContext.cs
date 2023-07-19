9f88a437cfcdb4edff3e25c465a5b28592cab1b6
Lime <demn2001@gmail.com>
2023-07-19 03:51:33 +0300

    hf

--- WebGP.Infrastructure/DataBase/ApplicationDbContext.cs
+++ WebGP.Infrastructure/DataBase/ApplicationDbContext.cs
@@ -5,11 +5,17 @@ namespace WebGP.Infrastructure.DataBase;
 
 public partial class DbContextGPO : BaseApplicationDbContext<DbContextGPO>, IContextGPO
 {
+    public DbContextGPO() { }
+    public DbContextGPO(DbContextOptions<DbContextGPO> options) : base(options) { }
+
     protected override void OnModelCreatingAbstract(ModelBuilder modelBuilder) => OnModelCreatingPartial(modelBuilder);
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
 public partial class DbContextGPC : BaseApplicationDbContext<DbContextGPC>, IContextGPC
 {
+    public DbContextGPC() { }
+    public DbContextGPC(DbContextOptions<DbContextGPC> options) : base(options) { }
+
     protected override void OnModelCreatingAbstract(ModelBuilder modelBuilder) => OnModelCreatingPartial(modelBuilder);
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
